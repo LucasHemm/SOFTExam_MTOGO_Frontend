70955d4d0f533ae250ce5b58be00b643dd62164e
Matias Rottbøll <mrottboell@gmail.com>
2024-11-25 16:00:17 +0100

    Update App.jsx

--- mtogo/src/App.jsx
+++ mtogo/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-import { useState } from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import RestaurantOverview from './Components/UserComponents/RestaurantOverview.jsx';
 import CreateUser from "./Components/UserComponents/CreateUser.jsx";
 import Login from "./Components/UserComponents/Login.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RestaurantMenu from "./Components/UserComponents/RestaurantMenu.jsx";
+import AgentOverview from "./Components/UserComponents/AgentOverview.jsx";
 
 
 
@@ -38,7 +39,8 @@ function App() {
             {showLogin && <Login setUser={setUser} setShowLogin={setShowLogin} />}
             {showRegister && <CreateUser setUser={setUser} setRegister={setShowRegister} />}
             {user != null && user.customerId > 0 && !restaurant && <RestaurantOverview restaurant={restaurant} setRestaurant={setRestaurant}/>}
-            {user != null && user.customerId > 0 && restaurant && <RestaurantMenu user={user} res={restaurant}/>}
+            {user != null && user.customerId > 0 && restaurant && <RestaurantMenu user={user} res={restaurant} setRes={setRestaurant}/>}
+            {user != null && user.agentId > 0 && <AgentOverview user={user} />}
 
 
         </>
